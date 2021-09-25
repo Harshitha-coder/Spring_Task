a1f8752d18e04c0b47ddd090cf11b81e8404924c
Harshitha1308 <harshithak794@gmail.com>
2021-09-25 21:45:05 +0530

    binding requestParam and model

--- demo-mvc/src/main/java/com/xworkz/demo/controller/HarshithaController.java
+++ demo-mvc/src/main/java/com/xworkz/demo/controller/HarshithaController.java
@@ -1,30 +1,50 @@
 package com.xworkz.demo.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-@RequestMapping
+import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Component
+@RequestMapping("/")
 public class HarshithaController {
 
-	
 	public HarshithaController() {
-	System.out.println("controller created");
+		System.out.println("controller created");
 	}
-	
-	@RequestMapping(value="/Harshitha.com")
-	public void onClicked()
-	{
+
+	@RequestMapping(value = "/Harshitha.com")
+	public String onClicked(@RequestParam String actorName, @RequestParam String actressName,
+			@RequestParam String movieName, @RequestParam String directorName,Model model) {
 		System.out.println("invoked onClicked");
+		System.out.println("actor name is:" + actorName);
+		System.out.println("actress name is:" + actressName);
+		System.out.println("movie name is:" + movieName);
+		System.out.println("director name is:" + directorName);
+		
+		model.addAttribute("actorName", actorName);
+		model.addAttribute("actressName", actressName);
+		model.addAttribute("movieName", movieName);
+		model.addAttribute("directorName", directorName);
+		
+		return "webresponse";
+		
 	}
-	@RequestMapping(value="/Ranjitha.com")
-	public void onClicked1()
-	{
+
+	@RequestMapping(value = "/Ranjitha.com")
+	public void onClicked1() {
 		System.out.println("invoked onClicked1");
 	}
-	@RequestMapping(value="/Navya.com")
-	public void onClicked2()
-	{
+
+	@RequestMapping(value = "/Navya.com")
+	public void onClicked2() {
 		System.out.println("invoked onClicked2");
+	}
+	
+	@RequestMapping(value="/getName.com")
+	public String getName() {
+		System.out.println("invoked getName");
+		return "webresponse";
 	}
 }
--- demo-mvc/src/main/webapp/index.jsp
+++ demo-mvc/src/main/webapp/index.jsp
@@ -13,7 +13,20 @@
 <h1><a href="Harshitha.com">Harshitha</a>
 <a href="Ranjitha.com">Ranjitha</a>
 <a href="Navya.com">Navya</a>
+<a href="getName.com">get</a>
 </h1>
 </pre>
+
+<div align="center">
+<form action="Harshitha.com">
+<pre>
+actor<input type="text" name="actorName">
+actress<input type="text" name="actressName">
+movie<input type="text" name="movieName">
+director<input type="text" name="directorName">
+<input type="submit" value="submit">
+</pre>
+</form>
+</div>
 </body>
 </html>
